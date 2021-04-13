37fea671fb3436e7c49f3be6f533cd84a9011be6
Natalie Cooper <natalie.cooper@nhm.ac.uk>
2021-04-13 19:01:12 +0100

    Update data wrangling and taxonomy scripts

--- data-wrangling/01B-taxonomy-check.R
+++ data-wrangling/01B-taxonomy-check.R
@@ -1,82 +1,84 @@
 # Taxonomy check using AmphibiaWeb taxonomy and Reptile database
+# April 2021
 
+#-----------------------
+# Prep
+#-----------------------
 # Load libraries
 library(tidyverse)
+
+# Read in our tidied data 
+ds2 <- read_csv("raw-data/halfwaydone.csv")
+
+# Split into amphibians and reptiles
+# And select only the species and family
+amphibians <- 
+  ds2 %>%
+  filter(class == "Amphibians") %>%
+  select(binomial, family)
+
+reptiles <- 
+  ds2 %>%
+  filter(class == "Reptiles") %>%
+  select(binomial, family)
 
 #----------------------------------
 # AMPHIBIANS
 #----------------------------------
-# Read in our list of species
-amp <- 
-  ds2 %>%
-  filter(class == "Amphibians") %>%
-  select(binomial, family) %>%
-  distinct()
-  
 # Read in check list from AmphibiaWeb
 # https://amphibiaweb.org/amphib_names.txt
-amphibian_species <- read_delim("raw-data/amphib_names.txt", delim = "\t")
+amphibians_frost <- read_delim("raw-data/amphib_names.txt", delim = "\t")
 
-# Add binomial column
-# Rename family to familyX so it is retained in joins
+# Add binomial_frost column and binomial column
+# We will join based on binomial, but then retain binomial_frost to correct
+# names manually.
+# Rename family to family_frost so it is retained in joins
 # Select only required columns
-amphibian_species <-
-  amphibian_species %>%
-  mutate(binomial = paste(genus, species)) %>%
-  rename(familyX = family) %>%
-  select(familyX, binomial)
+amphibians_frost <-
+  amphibians_frost %>%
+  mutate(binomial_correct = paste(genus, species)) %>%
+  mutate(binomial = binomial_correct) %>%
+  rename(family_correct = family) %>%
+  select(binomial, family_correct, binomial_correct)
 
 # Join to cut out species found in AmphibiaWeb but not our data
 # but keep those found in our data and not AmphibiaWeb
-# these will need checking...
-all_amp <- left_join(amp, amphibian_species, by = "binomial")
+# these will need checking manually...
+all_amphibians <- left_join(amphibians, amphibians_frost, by = c("binomial" = "binomial")) %>% 
+  distinct()
 
-# Which family names need correcting?
-fix_family_amphibians <- filter(all_amp, family != familyX)
+# write_csv(all_amphibians, file = "raw-data/amphibian-taxonomy-corrections-to-complete.csv")
+# Now manually check any mismatches and update the family taxonomy to match Frost
+# then save file as amphibian-taxonomy-corrections.csv
 
-# Which species names need correcting?
-fix_species_amphibians <- filter(all_amp, is.na(familyX)) %>% arrange(binomial)
 #----------------------------------
 # REPTILES
 #----------------------------------
-# Read in our list of species
-rep <-   
-  ds2 %>%
-  filter(class == "Reptiles") %>%
-  select(binomial, family) %>%
-  distinct()
-
 # Read in species check list from Uetz
 # http://www.reptile-database.org/data/
-reptile_species <- read_csv("raw-data/reptile_checklist_2020_12.csv")
+reptiles_uetz <- read_csv("raw-data/reptile_checklist_2020_12.csv")
 
 # Rename Species to binomial
 # Separate out Familyetc column to get Family names
-# Rename family to familyX so it is retained in joins
+# We will join based on binomial, but then retain binomial_correct to correct
+# names manually.
+# Rename family to family_correct so it is retained in joins
 # Select only required columns
-### Note this gives a warning as the Family etc column is not of a standardised
+### Note this gives a warning as the Familyetc column is not of a standardised
 ### size, but it doesn't matter as the first element (family) is all we want
-reptile_species <-
-  reptile_species %>%
-  rename(binomial = Species) %>%
-  separate(Familyetc, sep = ",", into = c("family", "x", "y", "z")) %>%
-  rename(familyX = family) %>%
-  select(familyX, binomial)
+reptiles_uetz <-
+  reptiles_uetz %>%
+  rename(binomial_correct = Species) %>%
+  mutate(binomial = binomial_correct) %>%
+  separate(Familyetc, sep = ",", into = c("family_correct", "x", "y", "z")) %>%
+  select(binomial, family_correct, binomial_correct)
 
-# Join to cut out species found in Uetz but not our data
-# but keep those found in our data and not Eutz
-# these will need checking...
-all_rep <- left_join(rep, reptile_species, by = "binomial")
+# Join to cut out species found in Reptile Database but not our data
+# but keep those found in our data and not Reptile Database
+# these will need checking manually...
+all_reptiles <- left_join(reptiles, reptiles_uetz, by = c("binomial" = "binomial")) %>% 
+  distinct()
 
-# Which family names need correcting?
-fix_family_reptiles <- filter(all_rep, family != familyX)
-
-# Which species names need correcting?
-fix_species_reptiles <- filter(all_rep, is.na(familyX)) %>% arrange(binomial)
-
-# Fixes are in the script 01C-taxonomy-fix-herps
-
-# Get the code for corrections
-#for (i in 1:length(fix_species_amphibians$binomial)){
-#  print(paste0("mutate(binomial = str_replace(binomial, '", fix_species_amphibians$binomial[i], "', 'FIX')) %>%" ))
-#} 
+# write_csv(all_reptiles, file = "raw-data/reptile-taxonomy-corrections-to-complete.csv")
+# Now manually check any mismatches and update the family taxonomy to match Uetz
+# then save file as reptile-taxonomy-corrections.csv
