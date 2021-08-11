e239c5681c1e31cf044a1ad006de17aaf5b8f9e9
Natalie Cooper <ncooper@tcd.ie>
2021-08-11 16:32:45 +0100

    Add best/worst taxa tables

--- analyses/table-worst-best-taxa.R
+++ analyses/table-worst-best-taxa.R
@@ -16,38 +16,59 @@ library(xtable)
 specimens <- read_csv(here("data/all-specimen-data-2021-07.csv")) 
 
 #---------------------------------------------------------  
-# Table S1
+# Table S?
 #---------------------------------------------------------  
-# Orders summary
-ds_orders <-
+# Best and worst species
+best <-
   specimens %>%
-  group_by(class) %>%
-  add_count(binomial, name = "n") %>%
-  add_count(binomial, sex, name = "nn") %>%
-  filter(n >= 10 & sex == "Female") %>%
-  rename(female = nn) %>%
-  mutate(male = n - female) %>%
-  select(class, order, binomial, n, female, male) %>%
-  mutate(percentf = (female/n) * 100) %>%
-  distinct()
+  add_count(class, binomial, name = "n") %>%
+  add_count(class, binomial, sex, name = "nn") %>%
+  select(class, binomial, sex, n, nn) %>%
+  # Add in data for species with no males or no females
+  complete(sex, nesting(class, binomial, n), fill = list(nn = 0)) %>%
+  distinct() %>%
+  mutate(percent = round(nn/n*100, 2)) %>%
+  filter(sex == "Female" & n >= 10) %>%
+  arrange(class, -percent) %>%
+  filter(percent >=75) %>%
+  select(class, binomial, `n specimens` = n, 
+         `% female` = percent)
 
-sum_orders <-
-  ds_orders %>%
-  group_by(class, order) %>%
-  summarise('n species' = length(unique(binomial)),
-            'n specimens' = sum(n),
-            '% female' = round(median(percentf),2))
+worst <-
+  specimens %>%
+  add_count(class, binomial, name = "n") %>%
+  add_count(class, binomial, sex, name = "nn") %>%
+  select(class, binomial, sex, n, nn) %>%
+  # Add in data for species with no males or no females
+  complete(sex, nesting(class, binomial, n), fill = list(nn = 0)) %>%
+  distinct() %>%
+  mutate(percent = round(nn/n*100, 2)) %>%
+  filter(sex == "Female" & n >= 10) %>%
+  arrange(class, percent) %>%
+  filter(percent <=25) %>%
+  select(class, binomial, `n specimens` = n, 
+         `% female` = percent)
 
-table_orders <-
-  xtable(sum_orders,
-         caption = "Median percentages of female specimens within species with at least 
-                   10 specimens, or each order of amphibians and reptiles. 
-                   Orders with median percentages of female specimens of over 50% are in bold.",
-         label = "table-orders",
-         align = c("l", "l", "l", "c", "c", "c"),
+table_best <-
+  xtable(best,
+         caption = "Best with >=10 specimens over 75% female",
+         label = "table-best",
+         align = c("l", "l", "l", "c", "c"),
          digits = 2)
 
-print(table_orders, file = here("manuscript/figures/table-orders.tex"), 
+table_worst <-
+  xtable(worst,
+         caption = "Worst with >=10 specimens under 25% female",
+         label = "table-best",
+         align = c("l", "l", "l", "c", "c"),
+         digits = 2)
+
+print(table_best, file = here("manuscript/figures/table-best.tex"), 
+      include.rownames = FALSE, tabular.environment = 'longtable',
+      floating = FALSE, caption.placement = "top", include.colnames = TRUE,
+      sanatize.colnames.function = c("bold.cells"))
+
+print(table_worst, file = here("manuscript/figures/table-worst.tex"), 
       include.rownames = FALSE, tabular.environment = 'longtable',
       floating = FALSE, caption.placement = "top", include.colnames = TRUE,
       sanatize.colnames.function = c("bold.cells"))
